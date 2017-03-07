0f5e9168c2fc2954b03b58806c864507694834eb
ORohr <oliver.rohr@uni-duesseldorf.de>
2017-03-07 17:16:26 +0100

    PlayerFragment

--- MyApplication/app/src/main/java/com/example/waractivitycontroller/view/activity/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/waractivitycontroller/view/activity/MainActivity.java
@@ -1,23 +1,30 @@
-package com.example.waractivitycontroller.view;
+package com.example.waractivitycontroller.view.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import com.example.waractivitycontroller.R;
 import com.example.waractivitycontroller.view.controller.DrawerListAdapter;
+import com.example.waractivitycontroller.view.fragment.PlayerFragment;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle toggle;
     private ListView mDrawerList;
+
+    private static final String ACTIVE_FRAGMENT = "fragment";
+
+    private static final int DRAWER_OPTION_PLAYER = 0;
+    private static final int DRAWER_OPTION_WAR = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         ArrayList<String> drawerItems = new ArrayList<String>() {{
-            add("Dummy1");
-            add("Dummy2");
+            add("Spieler");
+            add("Kämpfe");
             add("Dummy3");
         }};
 
@@ -39,15 +46,15 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
 
-        /*FrameLayout mFrameLayout = (FrameLayout) findViewById(R.id.activity_main_contentFrame);
+        FrameLayout mFrameLayout = (FrameLayout) findViewById(R.id.activity_main_container);
         if (mFrameLayout != null) {
             if (savedInstanceState != null) {
                 return;
             }
-            getSupportFragmentManager().beginTransaction().add(R.id.activity_main_contentFrame, new QueryFragment()).commit();
-        }*/
+            getSupportFragmentManager().beginTransaction().add(R.id.activity_main_container, new PlayerFragment()).commit();
+        }
 
-        toggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
 
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
@@ -72,6 +79,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void selectItem(int position) {
-        //Do stuff
+        Fragment fragment;
+        switch(position) {
+            case DRAWER_OPTION_PLAYER:
+                fragment = new PlayerFragment();
+                break;
+            case DRAWER_OPTION_WAR:
+                fragment = new PlayerFragment();
+                break;
+            default:
+                fragment = new PlayerFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_main_container, fragment, ACTIVE_FRAGMENT)
+                .commit();
+
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+
     }
 }
