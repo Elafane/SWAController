ac04673f831c048307425bbc751ac1e58c2dedd6
ORohr <oliver.rohr@uni-duesseldorf.de>
2017-03-08 10:32:19 +0100

    AddMonsterActivity

---
+++ MyApplication/app/src/main/java/com/example/waractivitycontroller/view/activity/AddMonsterActivity.java
@@ -0,0 +1,10 @@
+package com.example.waractivitycontroller.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Oliver
+ */
+
+public class AddMonsterActivity extends AppCompatActivity {
+}
